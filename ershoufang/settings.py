5d5d6d6303e0ff229f5e2423d9ce26903e1cc8ad
昕度 <xindu.zwb@alibaba-inc.com>
2017-04-10 08:39:57 +0800

    d

--- ershoufang/settings.py
+++ ershoufang/settings.py
@@ -28,13 +28,13 @@ NEWSPIDER_MODULE = 'ershoufang.spiders'
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-COOKIES_ENABLED = True
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
---
+++ ershoufang/spiders/ershoufanghz.py
@@ -0,0 +1,23 @@
+import scrapy
+from scrapy.http.request import Request
+
+class ershoufangSpider(scrapy.Spider):
+    name = "ershoufanghz"
+    start_urls = ["http://hz.lianjia.com/ershoufang/"]
+
+    def parse(self, response):
+        houses = response.xpath(".//ul[@class='sellListContent']/li")
+        for house in houses:
+            yield {
+                'region': house.xpath(".//div[@class='houseInfo']/a/text()").extract(),
+                'url':house.xpath(".//a[@class='img ']/@href").extract(),
+                'houseInfo':house.xpath(".//div[@class='houseInfo']/text()").extract(),
+                'unitPrice':house.xpath(".//div[@class='unitPrice']/span").re("\d+.\d+"),
+                'totalPrice':house.xpath(".//div[@class='totalPrice']/span").re("\d+.\d+")
+            }
+        page = response.xpath("//div[@class='page-box house-lst-page-box'][@page-data]").re("\d+")
+        if None != page and page[0] != page[1]:
+            next_page = "http://hz.lianjia.com/ershoufang/pg"+str(int(page[1])+1)
+            print next_page+"*********************"
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
