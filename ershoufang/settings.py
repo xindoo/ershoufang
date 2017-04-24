58dc7663e0eb91c69319f0b551f3a0d608578088
昕度 <xindu.zwb@alibaba-inc.com>
2017-04-24 20:49:55 +0800

    add url

--- ershoufang/settings.py
+++ ershoufang/settings.py
@@ -28,7 +28,7 @@ NEWSPIDER_MODULE = 'ershoufang.spiders'
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 20
+DOWNLOAD_DELAY = 30
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 2
 CONCURRENT_REQUESTS_PER_IP = 2
